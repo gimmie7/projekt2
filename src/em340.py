dd5a83595b77c3070bab9bf4dd1a9eda8b0da834
gimmie7 <mathias.rudolf@outlook.com>
2018-03-23 14:02:02 +0100

    add initial files and file structure

---
+++ src/em340.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import minimalmodbus
+import serial
+
+#Settings
+PORT = 'COM4'
+MODE = minimalmodbus.MODE_RTU
+DEBUG_MODE = True
+minimalmodbus.CLOSE_PORT_AFTER_EACH_CALL=True
+
+#Adapt this device to your OS and actual device path
+em340 = minimalmodbus.Instrument(PORT, 1, mode=MODE) # port name, slave address (in decimal)
+
+em340.serial.port          # this is the serial port name
+em340.serial.baudrate = 9600   # Baud
+em340.serial.bytesize = 8
+em340.serial.parity   = serial.PARITY_NONE
+em340.serial.stopbits = 1
+em340.serial.timeout  = 0.05   # seconds
+em340.mode = MODE   # rtu or ascii mode
+
+if DEBUG_MODE:
+    em340.debug = True
+
+## Read value from EM340
+
+try:
+    temperature = em340.read_register(288, 10) # Registernumber, number of decimals
+    print(temperature)
+except ValueError as err:
+    print("Failed to read from instrument em340")
+    print(err)
+
+## Write a config to the EM340
+#SOME_VALUE = 95
+#em340.write_register(24, SOME_VALUE, 1) # Registernumber, value, number of decimals for storage
